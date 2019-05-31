2764b17b0ea12717628a519d5f302f1fd80c7de8
Kevin Walton <kevywalton@gmail.com>
2019-05-31 07:48:25 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// $("p").click(function() {
+//     $(this).slideToggle("slow");
+// });
+
+// $("button").mouseenter(function() {
+//     $(this).addClass("makeRed").removeClass("makeBlue");
+// });
+
+// $("button").mouseleave(function() {
+//     $(this).removeClass("makeRed").addClass("makeBlue");
+// });
+
+$(document).ready(function() {
+    $(".box").on("click", function() {
+        
+        /*-- when we click on an element
+        that has a 'box' class, this event
+        will be fired --*/
+        
+        let classNames = $(this).attr("class").split(" ");
+        let boxClass = classNames[0];
+        let className = classNames[1];
+        if($(this).css("background-color") == "rgb(255, 0, 0)"){
+            
+            // if this object is already red, turn it black
+           $("." + classNames[1]).css("background-color", "#000"); 
+        } else {
+            // turn all elements with a box class black
+            // and then change the color of all elements
+            // with the same class as the clicked element
+            // to red
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+    });
+});
